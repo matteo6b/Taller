b2288043a9309560152a1ebd4b316b96214d25dd
mbistuer <m.bistuer.sallan@gmail.com>
2016-11-18 21:36:56 +0100

    inital commit

---
+++ src/Registro.java
@@ -0,0 +1,48 @@
+/**
+ * Created by Mateo on 18/11/16.
+ */
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Registro {
+    private Set<Coche> coches = new HashSet<>();
+
+    public void registrarVehiculo(Coche coche){
+        coches.add(coche);
+    }
+
+    public Optional<Coche> obtenerVehiculo(String matricula){
+        Coche cocheRetornar = null;
+        for (Coche asdf: coches){
+            if (asdf.getMatricula().equals(matricula)){
+                cocheRetornar = asdf;
+            }
+        }
+        if (cocheRetornar != null)
+            return Optional.ofNullable(cocheRetornar);
+        else
+            return null;
+    }
+
+    public void eliminarVehiculo(String matricula){
+        Coche abc = null;
+        for (Coche asdf: coches){
+            if (asdf.getMatricula().equals(matricula)){
+                abc = asdf;
+            }
+        }
+        coches.remove(abc);
+    }
+
+    public Optional<Coche> obtenerVehiculoPrecioMax(){
+        return Optional.ofNullable(Collections.max(coches, Comparator.comparing(Coche::getPrecio)));
+    }
+
+    public List<Coche> obtenerVehiculosMarca(String marca){
+        return coches.stream().filter(coche -> coche.getMarca().equals(marca)).collect(Collectors.toList());
+    }
+
+    public List<Coche> obtenerTodos(){
+        return new ArrayList<Coche>(coches);
+    }
+}
---
+++ src/Taller.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Mateo on 18/11/16.
+ */
+import java.util.*;
+
+public class Taller {
+    private Map<Persona, Coche> reparaciones = new TreeMap<>(Comparator.comparing(Persona::getNumSeguridadSocial));
+    public Coche registrarReparacion(Persona persona, Coche coche){
+        reparaciones.put(persona, coche);
+        return coche;
+    }
+    public Set<Persona> obtenerClientes(){
+        Set<Persona> set = reparaciones.keySet();
+        return set;
+    }
+    public Coche obtenerCoche(Persona persona){
+        return reparaciones.get(persona);
+    }
+
+
+}
